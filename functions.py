783fb3f874ef5ef34905ea4b79e8c274b52ad4a0
Aravindchary18 <aravindchary2007@gmail.com>
2026-01-30 21:38:33 +0530

    Add functions examples
    Demonstrate functions

---
+++ functions.py
@@ -0,0 +1,18 @@
+
+def hello(name,age):
+    print("broooo"+name,"you age is isi\t"+age)
+    
+hello("yiyi","01")
+hello("emmatladuthunnav ramada chow\t","99")
+#we choose one name in function it acts as default , like when you didnt gave name in the function bracket it gives your asnwer which you have entered in the def function like below
+def yo(name="yoyo",age=91):
+    
+    print("eheh"+name,"age is"+str(age ))
+
+yo()
+print("dig")
+
+def fih(name="thom"):
+    print("yo"+name+"!")
+    return name ,"sid" , 90
+print(fih())
