c2ab7f87da8c009e9c4c7e2232f82b64f6a4d46f
Aravindchary18 <aravindchary2007@gmail.com>
2026-01-31 17:35:26 +0530

    Add module usage examples
    Import and use a module

---
+++ modules.py
@@ -0,0 +1,18 @@
+# MODULES (Very Simple Explanation) read this there are also some properties ex- math for math utilities os from operating systems, re-regular expressions
+#note note note - read all these, and go watch youtube video of freecodecamp if you want the properties he mentioned them properly or you can also google them
+# A module is just another Python file.
+# We use modules to take code from another file.
+# First, we import the file using its file name.
+# Example: import tools
+# After importing, we must use the file name to call its function.
+# Example: tools.greet()
+# greet() is the function name written inside tools.py.
+# tools.greet() means: run greet() from the tools file.
+# The dot (.) connects the file name and the function name.
+# () means we are calling (running) the function.
+# Modules work like this: filename.function()
+# No class or object is needed to use a module function.
+#note-if you have any spaces in file name just keep underscore instead of leaving space when mentioning in module like hello_aravind. like this
+# we dont place colon :  in the end of module
+from aravind import vishnu
+vishnu()
