30c111bc127da82b4cd3600df3843af46c9c8bb5
Aravindchary18 <aravindchary2007@gmail.com>
2026-01-31 17:30:08 +0530

    Add variable scope examples
    Demonstrate local vs global variables

---
+++ variable scope.py
@@ -0,0 +1,6 @@
+#variable means , this is just like a typedef.that is if a variable with a memory saved outside a function it can be used anywhere in the function but if it is in the function it can be only used once.
+age=45
+def you():
+    print(age)
+
+you()
