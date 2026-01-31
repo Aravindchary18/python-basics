248c9ca41bbb738d839ce0555c3fbb7f8f12d6e9
Aravindchary18 <aravindchary2007@gmail.com>
2026-01-31 17:31:54 +0530

    Add objects examples

---
+++ objects.py
@@ -0,0 +1,10 @@
+#these are type of code that these are complex. that means these contain real and imaginary in it.note-complex doesnt contain bit length
+age=88
+print(age.real)
+print(age.imag)
+print(age.bit_length())
+items=["gig","pig"]
+items.append(4)
+items.pop()
+print(items)
+print(id(items))    
